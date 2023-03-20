51b9d52f947966026e60c4d225932afced278a6f
scottnotscott <brae@outlook.com>
2023-03-20 16:48:51 +0000

    cleanup prior to device specific layout fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,8 @@
 
 import Layout from "@/components/layout";
-import Image from "next/image";
-import { useRouter } from 'next/router';
-import { useSession } from "next-auth/react"
-import { ArrowDownLeft } from "lucide-react"
+
 
 export default function Home() {
-  const {data: session, status} = useSession();
-  const router = useRouter();
-
-  if (session && status == "authenticated") {
-    //router.replace('../dashboard');
-  }
   
   return (
     <Layout>
@@ -21,12 +12,8 @@ export default function Home() {
       </div>
       <div className="mt-8"></div>
       <div className="text-white pl-24">
-      <a href="http://localhost:3000/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F">Log in</a>
-       
-        
-       
+      <a href="http://localhost:3000/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F">Developer login only. If you are on prod use bottom left sign in link ;)</a>
        </div>
-       
     </Layout>
   );
   
