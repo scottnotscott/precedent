f282490671bda81366db3603d7ca956a275cd170
scottnotscott <brae@outlook.com>
2023-11-24 14:15:43 +0000

    just need this to deploy dont care about content

--- pages/colosseum.js
+++ pages/colosseum.js
@@ -1,17 +1,17 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Monsters from "@/components/monsters";
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import Monsters from "@/components/monsters";
 
-export default function Colosseum({ userStats, session }) {
-  const router = useRouter();
+// export default function Colosseum({ userStats, session }) {
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (!userStats || !session) {
-      router.push('/');
-    }
-  }, [userStats, session]);
+//   useEffect(() => {
+//     if (!userStats || !session) {
+//       router.push('/');
+//     }
+//   }, [userStats, session]);
 
-  return (
-    userStats && session && <Monsters userStats={userStats} session={session} />
-  );
-}
+//   return (
+//     userStats && session && <Monsters userStats={userStats} session={session} />
+//   );
+// }
--- pages/scout.js
+++ pages/scout.js
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Monsters from "@/components/monsters";
-import Scout from "@/components/Scout";
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import Monsters from "@/components/monsters";
+// import Scout from "@/components/Scout";
 
-export default function Scout({ userStats, session }) {
-  const router = useRouter();
+// export default function Scout({ userStats, session }) {
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (!userStats || !session) {
-      router.push('/');
-    }
-  }, [userStats, session]);
+//   useEffect(() => {
+//     if (!userStats || !session) {
+//       router.push('/');
+//     }
+//   }, [userStats, session]);
 
-  return (
-    userStats && session && <Monsters userStats={userStats} session={session} />
-  );
-}
+//   return (
+//     userStats && session && <Monsters userStats={userStats} session={session} />
+//   );
+// }
